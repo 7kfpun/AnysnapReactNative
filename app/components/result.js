63f3d8d2c97da848af56be9fb0aecd2d1537e0fc
kf <7kfpun@gmail.com>
2016-08-20 18:41:56 +0800

    Upload image to google cloud storage

--- app/components/result.js
+++ app/components/result.js
@@ -12,6 +12,7 @@ import {
 // 3rd party libraries
 import DeviceInfo from 'react-native-device-info';
 import ImageResizer from 'react-native-image-resizer';  // eslint-disable-line import/no-unresolved
+import RNFetchBlob from 'react-native-fetch-blob';
 import SafariView from 'react-native-safari-view';
 
 import Reactotron from 'reactotron';  // eslint-disable-line import/no-extraneous-dependencies
@@ -19,6 +20,10 @@ import Reactotron from 'reactotron';  // eslint-disable-line import/no-extraneou
 import firebase from 'firebase';
 
 import * as api from '../api';
+
+import { config } from '../config';
+
+const gcloudStorage = config.firebase.storageBucket;
 
 const uniqueID = DeviceInfo.getUniqueID();
 
@@ -40,12 +45,25 @@ export default class ResultView extends Component {
   constructor(props) {
     super(props);
 
+    let filename;
+    try {
+      filename = /id=(.*)\&ext/i.exec(this.props.image)[0].replace('id=', '').replace('&ext', '');  // eslint-disable-line no-useless-escape
+    } catch (err) {
+      filename = this.props.image.replace(/^.*[\\\/]/, '').replace('.jpg', '');
+    }
+
     this.state = {
+      filename,
       loading: true,
     };
   }
 
   componentDidMount() {
+    this.craftarSearch();
+    this.uploadImage();
+  }
+
+  craftarSearch() {
     const that = this;
     ImageResizer.createResizedImage(this.props.image, 600, 600, 'JPEG', 40).then((resizedImageUri) => {
       Reactotron.log({ log: 'Image resized', resizedImageUri });
@@ -67,20 +85,40 @@ export default class ResultView extends Component {
           that.setState({ empty: true });
         }
 
-        let filename;
-        try {
-          filename = /id=(.*)\&ext/i.exec(this.props.image)[0].replace('id=', '').replace('&ext', '');  // eslint-disable-line no-useless-escape
-        } catch (err) {
-          filename = this.props.image.replace(/^.*[\\\/]/, '').replace('.jpg', '');
-        }
-        firebase.database().ref(`app/img/${filename}/timestamp`).set(new Date().getTime());
-        firebase.database().ref(`app/img/${filename}/uniqueID`).set(uniqueID);
-        firebase.database().ref(`app/img/${filename}/original`).set(this.props.image);
-        firebase.database().ref(`app/craftar/${filename}`).set(json);
+        firebase.database().ref(`app/img/${this.state.filename}/timestamp`).set(new Date().getTime());
+        firebase.database().ref(`app/img/${this.state.filename}/uniqueID`).set(uniqueID);
+        firebase.database().ref(`app/img/${this.state.filename}/original`).set(this.props.image);
+        firebase.database().ref(`app/craftar/${this.state.filename}`).set(json);
       })
       .catch((error) => {
         Reactotron.log({ error });
       });
+    });
+  }
+
+  uploadImage() {
+    Reactotron.log(`https://www.googleapis.com/upload/storage/v1/b/${gcloudStorage}/o?uploadType=media&name=${this.state.filename}`);
+
+    RNFetchBlob.fetch(
+      'POST',
+      `https://www.googleapis.com/upload/storage/v1/b/${gcloudStorage}/o?uploadType=media&name=${this.state.filename}`,
+      {
+        'Content-Type': 'image/jpeg',
+      },
+      RNFetchBlob.wrap(this.props.image)
+    )
+    .then((response) => response.json())
+    .then((json) => {
+      Reactotron.log({ log: 'Uploaded image', json });
+
+      try {
+        firebase.database().ref(`app/bucket/${this.state.filename}`).set(json);
+      } catch (err) {
+        console.warn(err);
+      }
+    })
+    .catch((error) => {
+      console.warn(error);
     });
   }
 
