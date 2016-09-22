4e5326eac6f7b369181e366bac754248a036830a
kf <7kfpun@gmail.com>
2016-09-22 15:57:23 +0800

    Upgrate react native to 0.33.0

--- android/app/src/main/java/com/anysnap/MainApplication.java
+++ android/app/src/main/java/com/anysnap/MainApplication.java
@@ -4,14 +4,15 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.imagepicker.ImagePickerPackage;
-import com.react.rnspinkit.RNSpinkitPackage;
-import com.i18n.reactnativei18n.ReactNativeI18n;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import fr.bamlab.rnimageresizer.ImageResizerPackage;
-import com.RNFetchBlob.RNFetchBlobPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.react.rnspinkit.RNSpinkitPackage;
 import com.joshblour.reactnativepermissions.ReactNativePermissionsPackage;
+import fr.bamlab.rnimageresizer.ImageResizerPackage;
+import com.imagepicker.ImagePickerPackage;
+import com.i18n.reactnativei18n.ReactNativeI18n;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -32,16 +33,18 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-           new MainReactPackage(),
-            new ImagePickerPackage(),
-            new RNSpinkitPackage(),
-            new ReactNativeI18n(),
-            new RNDeviceInfo(),
-            new ImageResizerPackage(),
-            new RNFetchBlobPackage(),
-            new VectorIconsPackage(),
-            new ReactNativePermissionsPackage(),
-            new RCTCameraPackage()
+          new RNSpinkitPackage(),
+          new MainReactPackage(),
+          new RCTCameraPackage(),
+          new VectorIconsPackage(),
+          new RNSpinkitPackage(),
+          new ReactNativePermissionsPackage(),
+          new ImageResizerPackage(),
+          new ImagePickerPackage(),
+          new ReactNativeI18n(),
+          new RNFetchBlobPackage(),
+          new RNDeviceInfo(),
+          new RCTCameraPackage()
       );
     }
   };
